4e35330a81115780e79147dd92d0ee568777db65
VashUber <71492611+VashUber@users.noreply.github.com>
2023-04-19 17:41:51 +0300

    REFACTOR: 2632

--- curry-2632.ts
+++ curry-2632.ts
@@ -1,20 +1,13 @@
 function curry(fn: Function): Function {
-  let length = fn.length;
   const arr: number[] = [];
 
   return function curried(...args: number[]) {
-    length -= args.length;
     arr.push(...args);
-    if (length === 0) {
+
+    if (arr.length === fn.length) {
       return fn(...arr);
     }
 
     return curried;
   };
 }
-
-function sum(a: number, b: number) {
-  return a + b;
-}
-const csum = curry(sum);
-console.log(csum(1)(2));
