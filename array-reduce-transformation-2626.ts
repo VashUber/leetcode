bb3ddfb422b2a022e996ebe79634af2fd82b4631
VashUber <71492611+VashUber@users.noreply.github.com>
2023-04-19 15:28:46 +0300

    feature: js few easy problems

---
+++ array-reduce-transformation-2626.ts
@@ -0,0 +1,9 @@
+type Fn = (accum: number, curr: number) => number;
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+  let res = init;
+  for (const num of nums) {
+    res = fn(res, num);
+  }
+  return res;
+}
