4151be9f0b99dc7487e1a284a5541d1690984f5a
VashUber <71492611+VashUber@users.noreply.github.com>
2023-04-20 14:46:16 +0300

    refactor: 2633

--- convert-object-to-json-string-2633.ts
+++ convert-object-to-json-string-2633.ts
@@ -1,15 +1,15 @@
 function jsonStringify(object: any): string {
-  if (typeof object === "object" && object !== null && !Array.isArray(object)) {
+  if (Array.isArray(object)) {
+    const arr = object.map((elem) => jsonStringify(elem)).join(",");
+    return `[${arr}]`;
+  }
+
+  if (typeof object === "object" && object !== null) {
     const arr = Object.keys(object)
       .map((key) => `"${key}":${jsonStringify(object[key])}`)
       .join(",");
 
     return `{${arr}}`;
-  }
-
-  if (Array.isArray(object)) {
-    const arr = object.map((elem) => jsonStringify(elem)).join(",");
-    return `[${arr}]`;
   }
 
   if (typeof object === "string") {
